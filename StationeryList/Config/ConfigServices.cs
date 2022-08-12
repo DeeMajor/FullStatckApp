ac0a36da1388a110d9c7f34c6de7ea77f5c6ce47
Andile Dumakude <andile.dumakude@kcpivot.co.za>
2022-08-12 09:56:23 +0200

    Sort service configurations

---
+++ StationeryList/Config/ConfigServices.cs
@@ -0,0 +1,20 @@
+﻿using StationeryList.Model;
+using StationeryList.Repository;
+using StationeryList.Service;
+
+namespace StationeryList.Config
+{
+    public class ConfigServices
+    {
+        public static void ConfigureServices(IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.Configure<Database>(
+                builder.Configuration.GetSection("DefaultConnectionString")
+                );
+
+            services.AddTransient<IItemsService, ItemData>();
+            services.AddTransient<IStationeryService, StationeryData>();
+            services.AddTransient<StoredProcedure>();
+        }
+    }
+}
