582add7f2c2d4eba1cfa3b17bf563f59aa762dfd
Dwane Hall <hall.dwane@gmail.com>
2023-08-16 15:27:16 +0200

    Add in small powershell script

---
+++ .pipeline/powershell.ps1
@@ -0,0 +1,17 @@
+$configuration = "release"
+$projectDir = ".\Stationery.Api"
+$uiDir = ".\Stationery.UI"
+$projectName = "StationeryApp"
+
+dotnet restore
+dotnet build --no-restore -c $configuration .\StationeryList.sln
+dotnet test --no-restore .\StationerAppTests\StationerAppTests.csproj
+dotnet publish --no-restore -c $configuration -p:PublishDir=.\publish $projectDir\Stationery.Api.csproj
+
+push-location $uiDir
+npm install
+npm run build
+pop-location
+copy-item -r -Path $uiDir\build -Destination $projectDir\publish\build
+
+Compress-Archive -Path $projectDir\publish\ -Destination .\$projectName
