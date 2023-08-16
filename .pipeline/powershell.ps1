a54be9ae96ebfe325a2e03f04a80bded7dc65fc4
Dwane Hall <hall.dwane@gmail.com>
2023-08-16 16:22:59 +0200

    Powershell updates

--- .pipeline/powershell.ps1
+++ .pipeline/powershell.ps1
@@ -1,12 +1,13 @@
+$ErrorActionPreference = "Stop"
 $configuration = "release"
 $projectDir = ".\Stationery.Api"
 $uiDir = ".\Stationery.UI"
 $projectName = "StationeryApp"
 
-dotnet restore
-dotnet build --no-restore -c $configuration .\StationeryList.sln
-dotnet test --no-restore .\StationerAppTests\StationerAppTests.csproj
-dotnet publish --no-restore -c $configuration -p:PublishDir=.\publish $projectDir\Stationery.Api.csproj
+dotnet restore --packages .\packages
+dotnet build --no-restore -c $configuration /p:WarningLevel=0 .\StationeryList.sln
+dotnet test --no-restore -c $configuration /p:WarningLevel=0 .\StationerAppTests\StationerAppTests.csproj
+dotnet publish --no-restore -c $configuration /p:PublishDir=.\publish $projectDir\Stationery.Api.csproj
 
 push-location $uiDir
 npm install
@@ -14,4 +15,4 @@ npm run build
 pop-location
 copy-item -r -Path $uiDir\build -Destination $projectDir\publish\build
 
-Compress-Archive -Path $projectDir\publish\* -Destination .\$projectName
+compress-archive -Path $projectDir\publish\* -Destination .\$projectName
